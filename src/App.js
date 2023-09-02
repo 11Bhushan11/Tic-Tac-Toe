fd31bf078b9aab9e28217d89aba2e4cf7a026106
Sunny Bhushan <105878283+11Bhushan11@users.noreply.github.com>
2023-09-02 18:58:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Tictactoe from './Components/Tictactoe';
+// import Tictactoe from './Components/Tictactoe';
+
+function App() {
+  return (
+    <>
+    <Tictactoe /> 
+    </>
+  );
+}
+
+export default App;
